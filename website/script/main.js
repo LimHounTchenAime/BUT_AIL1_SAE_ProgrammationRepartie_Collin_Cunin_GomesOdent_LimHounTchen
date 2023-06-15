7f655016525747e6c4fc467ad9e4fcac5bf094fa
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-06-15 16:40:16 +0200

    process Traffic api

--- website/script/main.js
+++ website/script/main.js
@@ -42,15 +42,29 @@ fetch("https://transport.data.gouv.fr/gbfs/nancy/station_information.json")
 
 
 
-// 2. Traffic problems
+// Traffic problems
 fetch("https://carto.g-ny.org/data/cifs/cifs_waze_v2.json")
   .then(response => {
-    response.json().then(data => {
-      for (let i = 0; i < response2.data.stations.length; i++) {
-        const lat = response2.data.stations[i].lat
-        const lon = response2.data.stations[i].lon
-        //Put a marker on the map
-        const marker = L.marker([lat, lon]).addTo(map);
+    response.json().then(json => {
+      console.log(json)
+
+      for (ind in json.incidents)
+      {
+        const coordinates = json.incidents[ind].location.polyline.split(" ");
+        console.log(ind)
+        console.log(coordinates)
+        // const lat = incident.
+        // const lon = json.data.stations[i].lon
+        //
+        console.log(parseFloat(coordinates[0]), parseFloat(coordinates[1]))
+        // const marker = L.marker(parseFloat(coordinates[0]), parseFloat(coordinates[1])).addTo(map);
       }
+
+      // for (let i = 0; i < json.data.stations.length; i++) {
+      //   const lat = json.data.stations[i].lat
+      //   const lon = json.data.stations[i].lon
+      //   //Put a marker on the map
+      //   const marker = L.marker([lat, lon]).addTo(map);
+      // }
     })
   })
