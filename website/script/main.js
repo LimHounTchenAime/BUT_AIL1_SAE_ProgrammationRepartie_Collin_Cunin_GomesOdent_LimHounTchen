38eae1f0b74105c4ee565956530bf90ca209a32d
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-06-15 16:03:53 +0200

    center the map on Nancy

--- website/script/main.js
+++ website/script/main.js
@@ -1,15 +1,33 @@
-fetch("https://transport.data.gouv.fr/gbfs/nancy/station_information.json")
-    .then(response => {
-        if (response.ok) {
-            response.json().then(response2 => {
-                // Create Leaflet frame
-                const map = L.map('map').setView([response2.data.stations[0].lat, response2.data.stations[0].lon], 13);
+let map;
 
-                // Use a map service to display in the Leaflet frame
-                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 19,
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                }).addTo(map);
+// Initialize and center the map on Nancy
+fetch("https://api-adresse.data.gouv.fr/search/?q=Nancy+54")
+  .then(response => {
+    response.json().then(json => {
+      // Check the location is found
+      let i = 0;
+      while (json.features[i] && json.features[i].properties.context !== "54, Meurthe-et-Moselle, Grand Est")
+        i ++;
+      const data = json.features[i] ? json.features[i] : "Localisation introuvable";
+      console.log(data);
+
+      // Create Leaflet frame using location data
+      const map = L.map('map').setView([data.geometry.coordinates[1], data.geometry.coordinates[0]], 13);
+
+      // Use a map service to display in the Leaflet frame
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
+
+    })
+  })
+
+// Available Velibs
+fetch("https://transport.data.gouv.fr/gbfs/nancy/station_information.json")
+.then(response => {
+  if (response.ok) {
+        response.json().then(response2 => {
                 for (let i = 0; i < response2.data.stations.length; i++) {
                     const lat = response2.data.stations[i].lat
                     const lon = response2.data.stations[i].lon
@@ -19,3 +37,20 @@ fetch("https://transport.data.gouv.fr/gbfs/nancy/station_information.json")
             })
         }
     })
+
+
+
+
+
+// 2. Traffic problems
+fetch("https://carto.g-ny.org/data/cifs/cifs_waze_v2.json")
+  .then(response => {
+    response.json().then(data => {
+      for (let i = 0; i < response2.data.stations.length; i++) {
+        const lat = response2.data.stations[i].lat
+        const lon = response2.data.stations[i].lon
+        //Put a marker on the map
+        const marker = L.marker([lat, lon]).addTo(map);
+      }
+    })
+  })
