f248767422776b7d28416025e16d4e37bfaaab22
Castorsorcier <castorsorcier@gmail.com>
2023-06-16 15:02:50 +0200

    résolution problèmes de synchronisation

--- website/script/main.js
+++ website/script/main.js
@@ -10,7 +10,7 @@ fetch("https://api-adresse.data.gouv.fr/search/?q=Nancy+54")
       while (json.features[i] && json.features[i].properties.context !== "54, Meurthe-et-Moselle, Grand Est")
         i ++;
       const data = json.features[i] ? json.features[i] : "Localisation introuvable";
-      console.log(data);
+      //console.log(data);
 
       // Create Leaflet frame using location data
       const map = L.map('map').setView([data.geometry.coordinates[1], data.geometry.coordinates[0]], 13);
@@ -36,32 +36,33 @@ fetch("https://api-adresse.data.gouv.fr/search/?q=Nancy+54")
                 _stations[i] = { ...response2.data.stations[i], ..._stations[i] };
               }
             })
+              //ajout d'infos depuis le serveur 2 et ajout stations
+              fetch("https://transport.data.gouv.fr/gbfs/nancy/station_information.json")
+                  .then(response => {
+                      if (response.ok) {
+                          response.json().then(response2 => {
+                              //icone des stations
+                              var icon = L.icon({
+                                  iconUrl: 'https://www.smavd.org/wp-content/uploads/elementor/thumbs/Velo-pnn5fiv0mued6phjixnq24y55dm7fy4y16518hf9te.png',
+                                  iconSize: [50, 30], // size of the icon
+                              });
+                              for (let i = 0; i < response2.data.stations.length; i++) {
+                                  //ajout des infos
+                                  _stations[i] = {...response2.data.stations[i], ..._stations[i]};
+                                  //ajout d'une station
+                                  const marker = L.marker([_stations[i].lat, _stations[i].lon], {icon: icon}).addTo(map);
+                                  marker.bindPopup(
+                                      "<b>Adresse</b> : " + _stations[i].address.toString() +
+                                      "<br><b>Vélos disponibles</b> : " + _stations[i].num_bikes_available.toString()
+                                      + "<br><b>Places de parking libres</b> : " + _stations[i].num_docks_available.toString());
+                              }
+                          })
+                      }
+                  })
           }
         })
 
-            //ajout d'infos depuis le serveur 2 et ajout stations
-            fetch("https://transport.data.gouv.fr/gbfs/nancy/station_information.json")
-                .then(response => {
-                    if (response.ok) {
-                        response.json().then(response2 => {
-                            //icone des stations
-                            var icon = L.icon({
-                                iconUrl: 'https://www.smavd.org/wp-content/uploads/elementor/thumbs/Velo-pnn5fiv0mued6phjixnq24y55dm7fy4y16518hf9te.png',
-                                iconSize: [50, 30], // size of the icon
-                            });
-                            for (let i = 0; i < response2.data.stations.length; i++) {
-                                //ajout des infos
-                                _stations[i] = {...response2.data.stations[i], ..._stations[i]};
-                                //ajout d'une station
-                                const marker = L.marker([_stations[i].lat, _stations[i].lon], {icon: icon}).addTo(map);
-                                marker.bindPopup(
-                                    "<b>Adresse</b> : " + _stations[i].address.toString() +
-                                    "<br><b>Vélos disponibles</b> : " + _stations[i].num_bikes_available.toString()
-                                    + "<br><b>Places de parking libres</b> : " + _stations[i].num_docks_available.toString());
-                            }
-                        })
-                    }
-                })
+
 
 
 
@@ -71,7 +72,7 @@ fetch("https://api-adresse.data.gouv.fr/search/?q=Nancy+54")
       fetch("https://carto.g-ny.org/data/cifs/cifs_waze_v2.json")
         .then(response => {
           response.json().then(json => {
-            console.log(json)
+            //console.log(json)
 
             //icone des stations
             const icon = L.icon({
@@ -82,7 +83,7 @@ fetch("https://api-adresse.data.gouv.fr/search/?q=Nancy+54")
 
             for (let incident of json.incidents)
             {
-              console.log(incident)
+              //console.log(incident)
               let coordinates = incident.location.polyline.split(" ");
               coordinates = coordinates.map(function (elt) {
                 return parseFloat(elt);
