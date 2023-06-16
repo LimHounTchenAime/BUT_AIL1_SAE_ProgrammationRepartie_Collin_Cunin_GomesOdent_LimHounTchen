320f0db07998287a2cbf1f676c531ff0d67e9be8
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-06-16 08:40:03 +0200

    Traffic markers icon + code simplification

--- website/script/main.js
+++ website/script/main.js
@@ -26,7 +26,7 @@ fetch("https://api-adresse.data.gouv.fr/search/?q=Nancy+54")
 
 
       // --- Available Velibs ---
-//ajout des infos depuis le serveur 1
+      //ajout des infos depuis le serveur 1
       fetch("https://transport.data.gouv.fr/gbfs/nancy/station_status.json")
         .then(response=>{
           if(response.ok){
@@ -45,7 +45,7 @@ fetch("https://api-adresse.data.gouv.fr/search/?q=Nancy+54")
                     if (response.ok) {
                         response.json().then(response2 => {
                             //icone des stations
-                            var greenIcon = L.icon({
+                            var icon = L.icon({
                                 iconUrl: 'https://www.smavd.org/wp-content/uploads/elementor/thumbs/Velo-pnn5fiv0mued6phjixnq24y55dm7fy4y16518hf9te.png',
 
                                 iconSize: [50, 30], // size of the icon
@@ -54,7 +54,7 @@ fetch("https://api-adresse.data.gouv.fr/search/?q=Nancy+54")
                                 //ajout des infos
                                 _stations[i] = {...response2.data.stations[i], ..._stations[i]};
                                 //ajout d'une station
-                                const marker = L.marker([_stations[i].lat, _stations[i].lon], {icon: greenIcon}).addTo(map);
+                                const marker = L.marker([_stations[i].lat, _stations[i].lon], {icon: icon}).addTo(map);
                                 marker.bindPopup(
                                     "<b>Adresse</b> : " + _stations[i].address.toString() +
                                     "<br><b>Vélos disponibles</b> : " + _stations[i].num_bikes_available.toString()
@@ -68,17 +68,27 @@ fetch("https://api-adresse.data.gouv.fr/search/?q=Nancy+54")
 
 
 
-// --- Traffic problems ---
+      // --- Traffic problems ---
       fetch("https://carto.g-ny.org/data/cifs/cifs_waze_v2.json")
         .then(response => {
           response.json().then(json => {
             console.log(json)
 
+            //icone des stations
+            const icon = L.icon({
+              iconUrl: 'assets/Logo_travaux_orange.svg',
+
+              iconSize: [50, 30], // size of the icon
+            });
+
             for (let ind in json.incidents)
             {
-              const coordinates = json.incidents[ind].location.polyline.split(" ");
+              let coordinates = json.incidents[ind].location.polyline.split(" ");
+              coordinates = coordinates.map(function (elt) {
+                return parseFloat(elt);
+              });
               // console.log(coordinates);
-              const marker = L.marker([parseFloat(coordinates[0]), parseFloat(coordinates[1])]).addTo(map);
+              const marker = L.marker(coordinates, {icon: icon}).addTo(map);
             }
 
           })
