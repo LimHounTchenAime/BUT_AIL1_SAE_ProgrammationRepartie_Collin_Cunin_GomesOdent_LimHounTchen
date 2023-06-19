1e9121703d342f82054511175d49a02237c5c0b6
Castorsorcier <castorsorcier@gmail.com>
2023-06-19 14:34:25 +0200

    Ajout des classes pour la requête http

---
+++ website/serviceRMIdonneesProb/DataService.java
@@ -0,0 +1,8 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface DataService extends Remote {
+    void registerRestaurant(String name, String address, String contact) throws RemoteException;
+    void registerHigherEducationEstablishment(String name, String address, String contact) throws RemoteException;
+    String fetchData() throws RemoteException;
+}
