1781034574ace1667429c987bfa6a52eadbbe9c9
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-06-20 17:03:45 +0200

    Proxy Handler

--- rmi-service/src/main/java/org/example/proxy/Proxy.java
+++ rmi-service/src/main/java/org/example/proxy/Proxy.java
@@ -2,8 +2,14 @@ package org.example.proxy;
 
 import com.sun.net.httpserver.*;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URL;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -38,7 +44,44 @@ public class Proxy {
 
     static class ProxyHandler implements HttpHandler {
         public void handle(HttpExchange exchange) throws IOException {
-            // Implémentez ici la logique pour gérer les requêtes HTTP récupérant les données bloquées
+            try {
+                // Récupération de l'URL demandée
+                URI requestURI = exchange.getRequestURI();
+                URL targetURL = new URL(requestURI.toString());
+
+                // Création d'une connexion HTTP(S) vers l'URL cible
+                HttpURLConnection connection = targetURL.getProtocol().equals("https")
+                        ? (HttpsURLConnection) targetURL.openConnection()
+                        : (HttpURLConnection) targetURL.openConnection();
+
+                // Transfert des données entre le client et le serveur cible
+                try (InputStream inputStream = connection.getInputStream()) {
+                    // Récupération du code de réponse et des en-têtes
+                    int responseCode = connection.getResponseCode();
+                    Headers responseHeaders = exchange.getResponseHeaders();
+                    responseHeaders.putAll(connection.getHeaderFields());
+
+                    // Envoi de la réponse au client
+                    exchange.sendResponseHeaders(responseCode, 0);
+                    try (OutputStream outputStream = exchange.getResponseBody()) {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = inputStream.read(buffer)) != -1) {
+                            outputStream.write(buffer, 0, bytesRead);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                // Gestion des erreurs
+                int errorCode = 500;
+                String errorMessage = "Erreur interne du serveur: " + e.getMessage();
+
+                exchange.sendResponseHeaders(errorCode, errorMessage.length());
+                try (OutputStream outputStream = exchange.getResponseBody()) {
+                    outputStream.write(errorMessage.getBytes());
+                }
+            }
         }
+
     }
 }
